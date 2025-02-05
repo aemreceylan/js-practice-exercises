afc5869d4cc3289f51b339488174797a4025357b
ahmet emre ceylan <ahmetemreceylan@gmail.com>
2025-02-05 17:16:41 +0300

    update

--- very simple web game with docker/start-server.bat
+++
@@ -1,4 +0,0 @@
-docker build -t node_server .
-docker run -p 8080:8080 --name c_node_server -d node_server 
-docker exec -it c_node_server powershell
-node server.js
--- very simple web game with docker/start-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t node_server .
-docker run -p 8080:8080 --name c_node_server -d node_server -e KEEP_SERVER_RUNNING=true 
-docker exec -it c_node_server /bin/bash
-node server.js
